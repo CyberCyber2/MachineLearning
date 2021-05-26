46ba961aa22cea5a223c2baa4b5d8f3daff0f0a1
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-05-26 07:58:45 -0400

    Create DiscordGen.py

---
+++ DiscordGen.py
@@ -0,0 +1,17 @@
+from aitextgen.TokenDataset import TokenDataset
+from aitextgen.tokenizers import train_tokenizer
+from aitextgen.utils import GPT2ConfigCPU
+from aitextgen import aitextgen
+
+file_name = "general2.txt"
+
+train_tokenizer(file_name)
+tokenizer_file = "aitextgen.tokenizer.json"
+config = GPT2ConfigCPU()
+ai = aitextgen(tokenizer_file=tokenizer_file, config=config)
+data = TokenDataset(file_name, tokenizer_file=tokenizer_file, block_size=64)
+ai.train(data, batch_size=8, num_steps=50000, generate_every=5000, save_every=5000)
+ai.generate(10, prompt="Joe:")
+ai2 = aitextgen(model_folder="trained_model",
+                tokenizer_file="aitextgen.tokenizer.json")
+ai2.generate(10, prompt="Joe:")
